b1862a19138060548fbfcb193175986942abd0a5
vat0128 <130121319+Vat0128@users.noreply.github.com>
2024-06-09 13:50:55 +0700

    xong cart

--- app/src/main/java/com/example/xuong_kotlin/CartActivity.kt
+++ app/src/main/java/com/example/xuong_kotlin/CartActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -18,6 +19,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -27,6 +31,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -61,9 +66,9 @@ fun Succes() {
 
     var listItem = remember {
         mutableStateListOf(
-            Item(R.drawable.item, "Greek salad ", "with baked salmon", "12.00"),
-            Item(R.drawable.item, "Greek salad ", "with baked salmon", "12.00"),
-            Item(R.drawable.item, "Greek salad ", "with baked salmon", "12.00"),
+            Item(R.drawable.sanpham1, "Greek salad ", "with baked salmon", "12.00"),
+            Item(R.drawable.sanpham1, "Greek salad ", "with baked salmon", "12.00"),
+            Item(R.drawable.sanpham1, "Greek salad ", "with baked salmon", "12.00"),
         )
     }
     Column(
@@ -103,19 +108,34 @@ fun Succes() {
                 )
             }
         }
-        LazyColumn() {
+        LazyColumn(
+            modifier = Modifier
+                .weight(2f)
+                .padding(top = 20.dp)
+        ) {
             items(listItem) { item ->
-                Row (modifier = Modifier.fillMaxWidth().height(82.dp)) {
+                Row(
+                    modifier = Modifier
+                        .padding(bottom = 25.dp)
+                        .fillMaxWidth()
+                        .height(82.dp)
+                ) {
                     Image(
                         painter = painterResource(id = item.url), contentDescription = "",
                         modifier = Modifier
                             .width(78.dp)
                             .height(82.dp)
                     )
-                    Column (modifier = Modifier.fillMaxWidth()){
-                        Row (modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween){
-                            Column (){
+                    Column(
+                        modifier = Modifier
+                            .padding(start = 10.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Column() {
                                 Text(
                                     text = "${item.name}",
                                     fontSize = 18.sp,
@@ -124,9 +144,10 @@ fun Succes() {
                                 )
                                 Text(
                                     text = "${item.secondName}",
-                                    fontSize = 18.sp,
-                                    fontWeight = FontWeight(800),
-                                    lineHeight = 19.sp
+                                    fontSize = 14.sp,
+                                    fontWeight = FontWeight(400),
+                                    lineHeight = 16.sp,
+                                    color = Color(0xff8C8A9D)
                                 )
                             }
                             Image(
@@ -136,9 +157,121 @@ fun Succes() {
                                     .size(20.dp)
                             )
                         }
+                        Row(
+                            modifier = Modifier
+                                .padding(top = 12.dp)
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = "${item.price}",
+                                fontSize = 10.sp,
+                                fontWeight = FontWeight(800),
+                                lineHeight = 14.sp,
+                                color = Color(0xffFE724C)
+                            )
+                            Row (
+                                modifier = Modifier.width(85.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically
+                            ){
+                                Image(
+                                    painter = painterResource(id = R.drawable.trudetail),
+                                    contentDescription = "",
+                                    modifier = Modifier.size(28.dp)
+                                )
+                                Text(
+                                    text = "02 ",
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight(100),
+                                    lineHeight = 19.sp,
+                                )
+                                Image(
+                                    painter = painterResource(id = R.drawable.congdetail),
+                                    contentDescription = "",
+                                    modifier = Modifier.size(28.dp)
+                                )
+                            }
+                        }
                     }
                 }
             }
+        }
+        Box (modifier = Modifier.weight(1f)){
+           Column {
+               Row (modifier = Modifier.padding(top=10.dp, bottom = 15.dp).fillMaxWidth(),
+                   horizontalArrangement = Arrangement.SpaceBetween){
+                   Row {
+                       Text(
+                           text = "Items: ",
+                           fontSize = 10.sp,
+                           fontWeight = FontWeight(400),
+                           lineHeight = 14.sp,
+                       )
+                       Text(
+                           text = "3",
+                           fontSize = 10.sp,
+                           fontWeight = FontWeight(800),
+                           lineHeight = 14.sp,
+                       )
+                   }
+                   Text(
+                       text = "36$",
+                       fontSize = 10.sp,
+                       fontWeight = FontWeight(800),
+                       lineHeight = 14.sp,
+                   )
+               }
+               Row(modifier = Modifier.fillMaxWidth()) {
+                   Text(
+                       text = "Address: ",
+                       fontSize = 10.sp,
+                       fontWeight = FontWeight(400),
+                       lineHeight = 14.sp,
+                   )
+                   Text(
+                       text = "121/45 streets 5 ward 3 block LinhXuan state ThuDuc City Hochiminh",
+                       fontSize = 10.sp,
+                       fontWeight = FontWeight(800),
+                       lineHeight = 14.sp,
+                       maxLines = 1
+                   )
+               }
+               Row (modifier = Modifier.padding(top=15.dp, bottom = 10.dp).fillMaxWidth()){
+                   Text(
+                       text = "Payment Method: ",
+                       fontSize = 10.sp,
+                       fontWeight = FontWeight(400),
+                       lineHeight = 14.sp,
+                   )
+                   Text(
+                       text = "MB Bank",
+                       fontSize = 10.sp,
+                       fontWeight = FontWeight(800),
+                       lineHeight = 14.sp,
+                       maxLines = 1
+                   )
+               }
+               Button(
+                   onClick = { /*TODO*/ }, modifier = Modifier
+                       .padding(top = 20.dp, bottom = 27.dp)
+                       .fillMaxWidth().height(48.dp),
+                   shape = RoundedCornerShape(7.dp),
+                   colors = ButtonDefaults.buttonColors(
+                       containerColor = Color(0xffFF7400)
+                   )
+               ) {
+                   Text(
+                       text = "Check out",
+                       fontSize = 16.sp,
+                       fontWeight = FontWeight(800),
+                       lineHeight = 18.sp,
+                       color = Color.White,
+
+                       )
+               }
+           }
         }
     }
 }
