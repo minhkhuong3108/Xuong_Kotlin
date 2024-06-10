eda12c371d5482bae2d26ead3279c15e05aacae3
ngoc-ruby <140912199+ngoc-ruby@users.noreply.github.com>
2024-06-11 00:33:35 +0700

    Mô tả thay đổi của bạn

---
+++ app/src/main/java/com/example/xuong_kotlin/ProductActivity.kt
@@ -0,0 +1,12 @@
+package com.example.xuong_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class ProductActivity :  ComponentActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
